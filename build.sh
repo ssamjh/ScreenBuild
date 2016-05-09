b54bbf465c2db1628ca9e43eafdf6cabd1730b9e
ssamjh <sam.howell.sjh@gmail.com>
2016-05-10 00:46:28 +1200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+#If you plan on running this script with cron, please uncomment and edit this line.
+#cd /dir/this/script/is/in
+screen -d -m sh steps.sh
+echo Please check the output1 folder (set as default) for the final jar file.
