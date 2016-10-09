5fc9e01db2128a9517793521e2bdef44cb1d38bc
ssamjh <sam.howell.sjh@gmail.com>
2016-10-09 15:45:34 +1300

    Updated to support Spigot 1.10.2

--- steps.sh
+++ steps.sh
@@ -3,7 +3,7 @@
 mkdir output1
 cd output1
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar
-java -jar BuildTools.jar --rev 1.9.4
+java -jar BuildTools.jar --rev 1.10.2
 
 
 
